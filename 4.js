24c8307407c41b9c6dc5c61f014367ec74750865
BhuvanKB1 <84310394+BhuvanKB1@users.noreply.github.com>
2021-06-10 19:21:19 +0530

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const event = new Date();
+const app = express();
+app.get('/', function (req, res) {
+    res.send('Hello, world!');
+})
+app.get('/time', function (req, res) {
+    res.send(`${event.toISOString()}`);
+})
+app.listen(process.env.port || 8080);
